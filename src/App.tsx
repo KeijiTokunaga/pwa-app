a0cfbbe6bd1e91032c799d009eca554cbaccad1f
Keiji Tokunaga <keizi81@gmail.com>
2022-02-21 00:34:01 +0900

    add firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { getTasks } from "./firebase";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+        <button onClick={getTasks}>AAAAAAAAA</button>
       </header>
     </div>
   );
